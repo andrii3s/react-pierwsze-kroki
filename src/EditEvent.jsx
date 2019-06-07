2f6f176c81675f02dfe77b66cbd58697ed7a9110
andrii.tysh <tysh.andr@gmail.com>
2019-06-07 23:38:51 +0200

    * Lesson 05 + uniqid + editEvent & saveEvent

--- src/EditEvent.jsx
+++ src/EditEvent.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import './EditEvent.css';
+//import { tsPropertySignature } from '@babel/types';
 
 const EditEvent = props => {
     return (
-        <div className="edit-event">
-            <div className="edit-event__input-group">
+        <div className="edit-event-panel">
+            <div className="edit-event-input-row">
                 <label htmlFor="name">name</label>
-                <input type="text" id="name" name="name" />
+                <input
+                    type="text"
+                    id="name"
+                    name="name" 
+                    value={props.name}
+                    onChange = {e => props.onInputChange({[e.target.name]: e.target.value})}
+                />
             </div>
-            <div className="edit-event__input-group">
+            <div className="edit-event-input-row">
                 <label htmlFor="hour">hour</label>
-                <input type="tel" id="hour" name="hour" />
+                <input
+                    type="tel"
+                    id="hour"
+                    name="hour" 
+                    value={props.hour}
+                    onChange = {e => props.onInputChange({[e.target.name]: e.target.value})}
+                />
             </div>
-            <div className="edit-event__input-group">
+            <div className="edit-event-input-row">
                 <label htmlFor="minute">minute</label>
-                <input type="tel" id="minute" name="minute" />
+                <input
+                    type="tel"
+                    id="minute"
+                    name="minute" 
+                    value={props.minute}
+                    onChange = {e => props.onInputChange({[e.target.name]: e.target.value})}
+                />
             </div>
-            <button onClick={() => props.onSave()}>Ok</button>
-            <button>Cancel</button>
+            <div className="buttons-panel">
+                <button onClick={() => props.onSave()}>Ok</button>
+                <button>Cancel</button>
+            </div>
         </div>
     )
 }
